98be705919bca35da9e91e81762e8ca665d0ef96
JZylber <zylberjulian@gmail.com>
2024-07-08 17:17:37 -0300

    feat: fetchData and PostData

--- front/socket.js
+++ front/socket.js
@@ -1,11 +1,24 @@
 const socket = io("http://localhost:3000");
 
 const send = (type, data) => {
-    socket.emit("event", { type, payload: data });
+  socket.emit("event", { type, payload: data });
 };
 
-const receive = (type, callback) => {
-    socket.on("event", (data) => {
-        if (data.type === type) callback(data.payload);
-    });
+const receive = (type, callback, once = false) => {
+  const event = once ? "once" : "on";
+  socket[event]("event", (data) => {
+    if (data.type === type) callback(data.payload);
+  });
+};
+
+const fetchData = (type, callback) => {
+  send(type, null);
+  receive(type, callback, true);
+};
+
+const postData = (type, data, callback = undefined) => {
+  send(type, data);
+  if (callback) {
+    receive(type, callback, true);
+  }
 };
