70f7bf67e541face870914afde847839b45a2592
nachovigilante <nachovigilante@gmail.com>
2024-08-05 09:54:47 -0300

    fix: minor fixes

--- handler.js
+++ handler.js
@@ -1,14 +1,13 @@
 const handleEvent = (type, data) => {
-  let result;
   if (type === "message") {
     console.log(`Mensaje recibido: ${data.msg}`);
-    result = { msg: `Mensaje recibido: ${data.msg}` };
+    return { msg: `Mensaje recibido: ${data.msg}` };
   } else if (type === "date") {
     const date = new Date();
-    result = `${date.getUTCDate()}/${date.getUTCMonth() + 1}`;
+    return `${date.getUTCDate()}/${date.getUTCMonth() + 1}`;
   }
 
-  return result;
+  throw new Error(`Tipo de evento no soportado: ${type}`);
 };
 
 export default handleEvent;
