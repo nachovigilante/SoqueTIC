a610b53a1abe71ce933572f9391561bf26742bc0
JZylber <zylberjulian@gmail.com>
2024-11-03 19:48:08 -0300

    feat(logging): logging in/out events and colored messages

--- server.js
+++ server.js
@@ -22,15 +22,32 @@ const io = new Server(server, {
 
 const handleEvent = (type, data) => {
     const handler = events[type];
-    if (handler !== undefined) return handler(data);
-    console.log(`Llegó un evento no soportado: ${type}`);
+    if (handler !== undefined) {
+        console.log(
+            `Llegó un evento: \x1b[32m'${type}'\x1b[0m con la siguiente información:\n\x1b[34m${JSON.stringify(
+                data,
+                null,
+                2
+            )}\x1b[0m`
+        );
+        const response = handler(data);
+        console.log(
+            `Se ejecutó el handler de \x1b[32m'${type}'\x1b[0m con la siguiente respuesta:\n\x1b[34m${JSON.stringify(
+                response,
+                null,
+                2
+            )}\x1b[0m`
+        );
+        return response;
+    }
+    console.log(`\x1b[31mLlegó un evento no soportado: '${type}'\x1b[0m`);
     return {
         error: `Evento no soportado en el backend: ${type}.\nRevisar que haya un onEvent apropiado en el backend y/o revisar que coincidan los tipos de mensajes.`,
     };
 };
 
 io.on("connection", (socket) => {
-    console.log("Se conectó un soquete");
+    console.log("\x1b[90mSe conectó un soquete\x1b[0m");
     socket.on("realTimeEvent", async (type, data, callback) => {
         const result = await handleEvent(type, data);
         callback(result);
@@ -44,18 +61,25 @@ io.on("connection", (socket) => {
         callback(result);
     });
     socket.on("disconnect", () => {
-        console.log("Se desconectó un soquete");
+        console.log("\x1b[90mSe desconectó un soquete\x1b[0m");
     });
 });
 
 const sendEvent = (type, data) => {
+    console.log(
+        `Enviando evento: \x1b[32m'${type}'\x1b[0m con la siguiente información:\n\x1b[34m${JSON.stringify(
+            data,
+            null,
+            2
+        )}\x1b[0m`
+    );
     io.emit("realTimeEvent", type, data);
 };
 
 const startServer = (PORT = 3000) => {
     server.listen(PORT, () => {
         console.log(
-            "Servidor prendido\nEscuchando eventos...\nPara parar el servidor, apretá Ctrl + C\n"
+            "\x1b[93mServidor prendido\n\x1b[0mEscuchando eventos...\n\x1b[90mPara parar el servidor, apretá Ctrl + C\n\x1b[0m"
         );
     });
 };
