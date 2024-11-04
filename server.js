ef4d5390ada751c40af32d02d6ff1d4eff876c43
JZylber <zylberjulian@gmail.com>
2024-11-03 23:23:18 -0300

    feat: improved error handling for RESTful events

--- server.js
+++ server.js
@@ -27,16 +27,20 @@ const handleEvent = async (type, data) => {
     if (handler !== undefined) {
         DEBUGMODE &&
             console.log(
-                `Llegó un evento: \x1b[32m'${type}'\x1b[0m con la siguiente información:\n\x1b[34m${JSON.stringify(
-                    data,
-                    null,
-                    2
-                )}\x1b[0m`
+                `Llegó un evento: \x1b[32m'${type}'\x1b[0m${
+                    data
+                        ? ` con la siguiente información:\n\x1b[34m${JSON.stringify(
+                              data,
+                              null,
+                              2
+                          )}\x1b[0m`
+                        : ""
+                }`
             );
         const response = await handler(data);
         DEBUGMODE &&
             console.log(
-                `Se ejecutó el handler de \x1b[32m'${type}'\x1b[0m con la siguiente respuesta:\n\x1b[34m${JSON.stringify(
+                `Se respondió al mensaje \x1b[32m'${type}'\x1b[0m con:\n\x1b[34m${JSON.stringify(
                     response,
                     null,
                     2
@@ -62,12 +66,32 @@ io.on("connection", (socket) => {
         callback(result);
     });
     socket.on("GETEvent", async (type, callback) => {
-        const result = await handleEvent(type, undefined);
-        callback(result);
+        try {
+            const result = await handleEvent(type, undefined);
+            callback(result);
+        } catch (e) {
+            callback({
+                status: 500,
+                message: `El servidor crasheó al mandar el mensaje '${type}'`,
+            });
+            throw e;
+        }
     });
     socket.on("POSTEvent", async (type, data, callback) => {
-        const result = await handleEvent(type, data);
-        callback(result);
+        try {
+            const result = await handleEvent(type, data);
+            callback(result);
+        } catch (e) {
+            callback({
+                status: 500,
+                message: `El servidor crasheó al mandar el mensaje '${type}' con la data:\n${JSON.stringify(
+                    data,
+                    null,
+                    2
+                )}`,
+            });
+            throw e;
+        }
     });
     socket.on("disconnect", () => {
         DEBUGMODE && console.log("\x1b[90mSe desconectó un soquete\x1b[0m");
@@ -90,7 +114,7 @@ const startServer = (PORT = 3000, DEBUG = true) => {
     DEBUGMODE = DEBUG;
     server.listen(PORT, () => {
         console.log(
-            `\x1b[93mServidor prendido\n\x1b[0mEscuchando eventos...\n\x1b[90mPara parar el servidor, apretá Ctrl + C\n${
+            `\x1b[93m¡Servidor prendido!\n\x1b[0mEscuchando eventos...\n\x1b[90mPara parar el servidor, apretá Ctrl + C\n${
                 DEBUG
                     ? "Para que no se muestren por consola los mensajes sobre el funcionamiento de \x1b[0mSoqueTIC\x1b[90m, iniciar \x1b[32mstartServer\x1b[90m con modo \x1b[34m'DEBUG'\x1b[90m en \x1b[32mfalse\n"
                     : ""
